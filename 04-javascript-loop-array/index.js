6b719ac4aa991280170aba46f3d29be2b0d1d5d8
danyawn <thephantomwarrio02@gmail.com>
2022-10-20 22:46:59 +0700

    submit 04-javascript-loop-array

---
+++ 04-javascript-loop-array/index.js
@@ -0,0 +1,79 @@
+// no 1
+console.log("");
+console.log("Jawaban No. 1");
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+// no 2
+console.log("");
+console.log("Jawaban No. 2");
+for (let index = 0; index < 10; index++) {
+  if (index % 2 != 0) {
+    console.log(index);
+  }
+}
+
+// no 3
+console.log("");
+console.log("Jawaban No. 3");
+for (let index = 0; index < 10; index++) {
+  if (index % 2 == 0) {
+    console.log(index);
+  }
+}
+
+// no 4
+console.log("");
+console.log("Jawaban No. 4");
+let array1 = [1, 2, 3, 4, 5, 6];
+console.log(array1[5]);
+
+// no 5
+console.log("");
+console.log("Jawaban No. 5");
+let array2 = [5, 2, 4, 1, 3, 5];
+array2.sort();
+console.log(array2);
+
+// no 6
+console.log("");
+console.log("Jawaban No. 6");
+let array3 = ["selamat", "anda", "melakukan", "perulangan", "array", "dengan", "for"];
+
+for (let index = 0; index < array3.length; index++) {
+  console.log(array3[index]);
+}
+
+// no 7
+console.log("");
+console.log("Jawaban No. 7");
+let array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let index = 0; index < array4.length; index++) {
+  if (array4[index] % 2 == 0) {
+    console.log(array4[index]);
+  }
+}
+
+// no 8
+console.log("");
+console.log("Jawaban No. 8");
+let kalimat = ["saya", "sangat", "senang", "belajar", "javascript"];
+let combine = kalimat.join(" ");
+console.log(combine);
+
+// no 9
+console.log("");
+console.log("Jawaban No. 9");
+var sayuran = [];
+var sayurnya = ["Kangkung", "Bayam", "Buncis", "Kubis", "Timun", "Seledri", "Tauge"];
+
+for (let index = 0; index < sayurnya.length; index++) {
+  sayuran.push(sayurnya[index]);
+}
+
+console.log(sayuran);
+
+// no 10
+console.log("");
+console.log("Jawaban No. 10");
