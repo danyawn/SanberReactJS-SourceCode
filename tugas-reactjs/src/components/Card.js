e4b8431e83ff5dca74ac4326d3fbf31e6e3529cc
danyawn <thephantomwarrio02@gmail.com>
2022-10-25 21:05:46 +0700

    submit tugas-reactjs

---
+++ tugas-reactjs/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <h2>HI, Saya Yan Danu</h2>
+      <hr />
+      <p>Saya dari Batch 39 sudah berhasil menginstall REACT</p>
+    </div>
+  );
+}
